3f65264d253fa70b8f56eb08c06329a8baae01d6
mike242424 <mikebyers24@gmail.com>
2024-02-02 13:52:17 -0500

    set up split bill controlled elements

--- src/components/SplitForm.jsx
+++ src/components/SplitForm.jsx
@@ -1,18 +1,30 @@
+import { useState } from 'react';
 import Button from './Button';
 
 export default function SplitForm({ selectedFriend }) {
+  const [bill, setBill] = useState('');
+  const [myExpense, setMyExpense] = useState('');
+  const [friendExpense, setFriendExpense] = useState('');
   return (
     <form className="form-split-bill">
       <h2>Split Bill with {selectedFriend.name}</h2>
 
       <label>💰 Bill Value:</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={bill}
+        onChange={(e) => setBill(e.target.value)}
+      />
 
       <label>🧍🏼‍♂️ Your expense:</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={myExpense}
+        onChange={(e) => setMyExpense(e.target.value)}
+      />
 
       <label>👨🏻‍🤝‍👨🏾 {selectedFriend.name}'s expense:</label>
-      <input type="text" disabled />
+      <input type="text" disabled value={bill - myExpense} />
 
       <label>🤑 Who is paying the bill?</label>
       <select>
